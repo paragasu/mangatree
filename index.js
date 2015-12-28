a5ef12fa6c68fd619e617bf22eace7c27192cd14
paragasu <paragasu@gmail.com>
2015-12-28 14:12:21 +0800

    add not bookmark notification

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var {ToggleButton} = require('sdk/ui/button/toggle');
-let {search} = require('sdk/places/bookmarks');
-var data     = require('sdk/self').data;
+let {search}       = require('sdk/places/bookmarks');
+var {data, version}= require('sdk/self');
+
 var tabs     = require('sdk/tabs');
 var Request  = require('sdk/request').Request;
 var promise  = require('sdk/core/promise');
 var Panel    = require('sdk/panel').Panel;
+var notify   = require('sdk/notifications');
 
 const mangaList  = {
 	'www.mangahen.com' : /(www\.mangahen\.com)\/(\w+)\/(\d+)\/(\d+)?\/?/i
@@ -16,7 +18,7 @@ const ICON = {
 }
 
 notAvailableList = {
-	'www.mangahen.com'    : /not available yet/i,
+	'www.mangahen.com'    : /(raw|not available yet)+/i,
 	'www.mangareader.net' : /not published yet/i
 }
 
@@ -28,15 +30,12 @@ var panel   = Panel({
 });
 
 panel.on('show', function(){
-
 	console.log('send port show signal');
 	panel.port.emit('show', JSON.stringify(updatedList));
 })
 
 panel.port.on('clicked', function(url){
-
 	console.log('link clicked ', url);
-
 	tabs.open(url);
 });
 
@@ -56,6 +55,11 @@ var button = ToggleButton({
 			var s = search([{ query: manga }], { sort: 'updated', decending: true });
 
 			s.on('end', function(results){
+
+				// no bookmark found
+				if(results.length === 0){
+					return showNoBookmarkFound();	
+				}
 
 				var bookmarks  = results.map(function(res){
 
@@ -156,3 +160,15 @@ var checkUpdate = function(item){
 	})
 }
 
+
+var showNoBookmarkFound = function(){
+
+	//restore action icon	
+	button.icon = ICON;
+
+	notify.notify({
+		title: 'Manga Tree v' + version,
+		iconURL: './icon-32.png',
+		text: 'No bookmark on ' + Object.keys(mangaList).join(',') + ' found.\n'
+	});	
+}
